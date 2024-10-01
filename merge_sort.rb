d8f3885a3aeffe44bc31632f7effb8a1a77c0560
Volodymyr Pivtoranis <pivtoranis87@gmail.com>
2024-10-01 22:28:36 +0300

    Implement merge method to further use in merge_sort

---
+++ merge_sort.rb
@@ -0,0 +1,26 @@
+# frozen_string_literal: true
+
+def merge_sort(arr)
+  return arr if arr.length == 1
+
+  mid = arr.length / 2
+
+  left = merge_sort(arr[0...mid])
+  right = merge_sort(arr[mid..])
+end
+
+def merge(arr1, arr2)
+  sorted_array = []
+
+  while !arr1.empty? && !arr2.empty?
+    sorted_array << if arr1[0] <= arr2[0]
+                      arr1.shift
+                    else
+                      arr2.shift
+                    end
+  end
+
+  sorted_array.concat(arr1).concat(arr2)
+end
+
+p merge([3], [1])
